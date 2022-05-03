e87ff37cdc4d4ea615b120920d079a7d5c7de8ad
KimMacVicar <92276525+KimMacVicar@users.noreply.github.com>
2022-05-02 20:03:12 -0600

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1> Weather App
+      <footer>
+        This project was created by Kim MacVicar and is{" "}
+        <a href="https://github.com/KimMacVicar/react-week-5" target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
