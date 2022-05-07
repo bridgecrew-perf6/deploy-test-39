abd7b4420cad88f50a43a0240b50ea281ad20c25
KimMacVicar <92276525+KimMacVicar@users.noreply.github.com>
2022-05-07 08:20:55 -0600

    added styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,29 +4,37 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="row">
-        <div className="col-10">
+      <div className="row search">
+        <div className="col-9">
           <form className="form-border">
             <input
               className="form-control"
               type="search"
               placeholder="Enter a city.."
+              autoFocus="on"
             />
           </form>
         </div>
-        <div className="col-2 btn-border">
-          <input className=" btn btn-primary " type="submit" value="search" />
+        <div className="col-3 btn-border">
+          <input
+            className=" btn btn-primary w-100"
+            type="submit"
+            value="search"
+          />
         </div>
       </div>
       <div className="row">
         <div className="col-3 weather-image-temp">
-          <img
-            className="weather-image"
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          <span className="temp">6</span>
-          °C
+          <div className="clearfix">
+            <img
+              className="weather-image "
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+            />
+
+            <span className="temp">6</span>
+            <span className="unit"> °C</span>
+          </div>
         </div>
         <div className="col-3 weather-info">
           <ul className="weather-info">
@@ -36,7 +44,7 @@ export default function Weather() {
           </ul>
         </div>
         <div className="col-6 place-info">
-          <h2>Martina Franca, Taranto, Italy</h2>
+          <h3>Martina Franca, Italy</h3>
           <ul>
             <li>Wednesday 07:00</li>
             <li>Mostly Cloudy</li>
