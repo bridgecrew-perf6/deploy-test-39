6ee9ee5986aec24a280964756fac3aa7881a9b9e
KimMacVicar <92276525+KimMacVicar@users.noreply.github.com>
2022-05-08 08:21:54 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-<link
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-  rel="stylesheet"
-  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-  crossorigin="anonymous"
-></link>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
